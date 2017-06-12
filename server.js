b042ca95d3aca64024dc832ea1adaf6c26f5ba4c
Jason Crawford <daxapps777@gmail.com>
2017-06-12 18:06:01 -0500

    Add basic quthentication and access control

--- server.js
+++ server.js
@@ -1,31 +1,57 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const {router: usersRouter} = require('./users');
 
 app.use(express.static('public'));
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+
+// logging
+app.use(morgan('common'));
+
+app.use('/users/', usersRouter);
+
+app.use('*', function(req, res) {
+  return res.status(404).json({message: 'Not Found'});
+});
 
 let server;
 
 function runServer() {
-	const port = process.env.PORT || 8080;
+	// const port = process.env.PORT || 8080;
 	return new Promise((resolve, reject) => {
-		server = app.listen(port, () => {
-			console.log(`Your app is listening on port ${port}`);
-			resolve(server);
-		}).on('error', err => {
-			reject(err)
-		});
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+  		server = app.listen(PORT, () => {
+  			console.log(`Your app is listening on port ${PORT}`);
+  			resolve(server);
+  		}).on('error', err => {
+        mongoose.disconnect();
+  			reject(err)
+  		});
+    });
 	});
 }
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });  
     });
   });
 }
@@ -36,6 +62,9 @@ if (require.main === module) {
 
 module.exports = {app, runServer, closeServer};
 
+app.get('/users', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
 
